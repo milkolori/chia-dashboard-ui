a0b8e47701762fbf60a61dbcaada69615f925604
Milko Lorinkov <milkolori@gmail.com>
2021-05-20 21:34:44 +0300

    client app setup

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,10 @@
-export const discordClientId = '834108043687231570';
+// export const discordClientId = '834108043687231570';
+export const discordClientId = null;
 // export const githubClientId = '16645067d609075c61e4';
 export const githubClientId = null;
-// export const googleClientId = '444774972531-atkv3k11o8satjic5m88ldv16beri3rf.apps.googleusercontent.com';
-export const googleClientId = null;
-export const apiBaseUrl = 'https://chia-dashboard-api.foxypool.io/api';
+export const googleClientId = '21726932377-iqh3tqjutfq5s7d4obll0fth54hhg8k2.apps.googleusercontent.com';
+// export const googleClientId = null;
+export const apiBaseUrl = 'https://lots-chia-core.herokuapp.com/api';
 export const enablePeriodicUpdates = true;
 export const requestDiscordGuildPermission = true;
 
